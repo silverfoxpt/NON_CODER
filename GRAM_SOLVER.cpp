ae71b86b15b4a795d9d81cefbc39c29e59e8b903
silverfoxpt <77423595+silverfoxpt@users.noreply.github.com>
2021-01-18 09:24:13 +0700

    New header files

--- GRAM_SOLVER.cpp
+++ GRAM_SOLVER.cpp
@@ -11,12 +11,30 @@
 #include <utility>
 #include <cmath>
 #include <climits>
+#include "LINE_SOLVER.h"
 using namespace std;
+
+int n; vector< vector<int> > rows, cols;
 
 int main()
 {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    //ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     ifstream inp("NON.INP");
+    for (int i = 1; i <= 10; i++)
+    {
+        vector<int> tmp;
+        inp >> n;
+        int x; for (int j = 1; j <= n; j++) {inp >> x; tmp.push_back(x);}
+        rows.push_back(tmp);
+    }
+    for (int i = 1; i <= 10; i++)
+    {
+        vector<int> tmp;
+        inp >> n;
+        int x; for (int j = 1; j <= n; j++) {inp >> x; tmp.push_back(x);}
+        cols.push_back(tmp);
+    }
+
 
 }
 
--- LINE_SOLVER.h
+++ LINE_SOLVER.h
@@ -1,6 +1,9 @@
 #ifndef FUNCTIONSEXAMPLE_H
 #define FUNCTIONSEXAMPLE_H
 
+#include <vector>
+using namespace std;
+
 bool checkLine(vector<int>a);
 vector<vector<int> > Try(int i, int counter, vector<int> a, vector<int>& pos, int n);
 
